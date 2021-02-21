3bdea36c010016be13f6bc7b3bf9c84f28803364
Negassa Berhanu <negassab16@gmail.com>
2021-02-21 04:29:24 +0100

    changed the loop time to 20 secs

--- chips_tracker.py
+++ chips_tracker.py
@@ -82,7 +82,7 @@ async def main():
             )
 
             logger.info("looping")
-            time.sleep(30)
+            time.sleep(20)
         except errors.FloodWaitError as e:
             logger.error(
                 f"Hit the flood-wait-error, Gotta sleep for {e.seconds} secs"
